9a7673af4432f9279c4c133338f9d810bf20390f
Stezsz <135887351+Stezsz@users.noreply.github.com>
2024-04-05 10:49:52 +0100

    Add files via upload

---
+++ 42 cursus/libft/ft_memcmp.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: strodrig <strodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 13:45:12 by strodrig          #+#    #+#             */
+/*   Updated: 2024/03/15 13:45:12 by strodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	unsigned char *str1;
+	unsigned char *str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] && str1[i] == str2[i] && i < n)
+		i++;
+	return (str1[i] - str2[i]);
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	int result;
+
+	result = 0;
+	result = ft_memcmp("12", "9", 20);
+
+	if (result > 0)
+		printf("str1 > str2");
+	else if (result < 0)
+		printf("str1 < str2");
+	else
+		printf("str1 == str2");
+	return (0);
+}*/
