435577932ae58d306b7dd48dccbc934cfedc0bb9
Stephan <strodrig@LAPTOP-JIMSQ8SG>
2024-11-26 17:31:43 +0000

    To deliver

--- push_swap/srcs/utils/ft_split.c
+++ push_swap/srcs/utils/ft_split.c
@@ -10,38 +10,72 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
+#include "push_swap.h"
+
+static int	count_words(const char *s, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			count++;
+			while (*s && *s != c)
+				s++;
+		}
+	}
+	return (count);
+}
+
+static char	*extract_word(const char *s, char c)
+{
+	int		len;
+	char	*word;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	len = 0;
+	while (s[len] && s[len] != c)
+	{
+		word[len] = s[len];
+		len++;
+	}
+	word[len] = '\0';
+	return (word);
+}
 
 char	**ft_split(const char *s, char c)
 {
 	char	**result;
 	int		i;
-	int		j;
-	int		k;
 
 	i = 0;
-	result = malloc(sizeof(char *) * 256);
+	result = malloc(sizeof(char *) * (count_words(s, c) + 1));
 	if (!result)
 		return (NULL);
 	while (*s)
 	{
 		while (*s == c && *s)
 			s++;
-		if (!*s)
-			break ;
-		j = 0;
-		while (s[j] && s[j] != c)
-			j++;
-		result[i] = malloc(j + 1);
-		if (!result[i])
-			return (NULL);
-		k = 0;
-		while (k < j)
-			result[i][k++] = *s++;
-		result[i++][k] = '\0';
+		if (*s)
+		{
+			result[i] = extract_word(s, c);
+			if (!result[i])
+				return (NULL);
+			i++;
+			while (*s && *s != c)
+				s++;
+		}
 	}
-	result[i] = (NULL);
+	result[i] = NULL;
 	return (result);
 }
 
