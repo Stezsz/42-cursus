64c9379ceae7df836975526428a9c50fd9a01fc6
Stephan Rodrigues Lassaponari <strodrig@c2r3s12.42lisboa.com>
2024-12-07 15:04:58 +0000

    Pre aval

--- push_swap/srcs/checks/get_input.c
+++ push_swap/srcs/checks/get_input.c
@@ -6,11 +6,19 @@
 /*   By: strodrig <strodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:13:35 by strodrig          #+#    #+#             */
-/*   Updated: 2024/12/07 12:50:36 by strodrig         ###   ########.fr       */
+/*   Updated: 2024/12/07 15:01:27 by strodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	free_error(t_stack *a, t_stack *b, char **split_args)
+{
+	free_stack(a);
+	free(b);
+	if (split_args)
+		free_split(split_args);
+}
 
 static char	**parse_input(char **av, int *size)
 {
@@ -38,7 +46,10 @@ void	get_input(t_stack *a, t_stack *b, char **av, int size)
 		av = split_args;
 	if (!is_integer(av, size) || is_greater_than_int(av, size)
 		|| is_duplicates(av, size))
+	{
+		free_error(a, b, split_args);
 		put_error("Error\n");
+	}
 	while (size)
 		push_elem(a, new_elem(ft_atoi(av[--size])));
 	if (is_sorted(*a))
