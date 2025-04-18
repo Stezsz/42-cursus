1110e14ffe28659ceca608aab34160b44abea41a
stephanrlassaponari@gmail.com <stephanrlassaponari@gmail.com>
2025-04-18 20:30:08 +0100

    Exams

--- Exams/exam_03/get_next_line/get_next_line.c
+++ Exams/exam_03/get_next_line/get_next_line.c
@@ -1,74 +1,104 @@
 #include "get_next_line.h"
 
-size_t ft_strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
-	size_t i = 0;
-	if(!str)
-		return 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	return (i);
+	size_t	counter;
+
+	counter = 0;
+	if (!str)
+		return (0);
+	while (str[counter] != '\n' && str[counter])
+		counter++;
+	counter += (str[counter] == '\n');
+	return (counter);
 }
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
-	size_t i = 0, j = 0;
+	int		a;
+	int		c;
 
-	res = malloc(ft_strlen(s1) + ft_strlen(s2));
+	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!res)
 		return (NULL);
-	while (s1 && s1[i])
-		res[j++] = s1[i++];
-	i = 0;
-	while (s2 && s2[i] != '\n')
-		res[j++] = s2[i++];
-	if (s2[i] == '\n')
-		res[j++] = '\n';
-	res[j] = 0;
+	a = 0;
+	c = 0;
+	while (s1 && s1[a])
+		res[c++] = s1[a++];
+	a = -1;
+	while (s2[++a] && s2[a] != '\n')
+		res[c++] = s2[a];
+	if (s2[a++] == '\n')
+		res[c++] = '\n';
+	res[c] = 0;
 	free(s1);
-	return res;
+	return (res);
 }
 
 void	bufflord(char *str)
 {
-	size_t i = 0, j = 0;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
-	if (str[i] == '\n')
-		i++;
+	i += (str[i] == '\n');
 	while (str[i])
-		str[j++] = str[i++];
+	{
+		str[j++] = str[i];
+		str[i++] = 0;
+	}
 	str[j] = 0;
 }
 
 char	*reader(int fd, char *buffer)
 {
-	char	*line = NULL;
+	char	*line;
+	int		i;
 
-	while(buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0)
+	i = 0;
+	line = NULL;
+	while (buffer[0] || read(fd, buffer, BUFFER_SIZE))
 	{
 		line = ft_strjoin(line, buffer);
-		if (!line)
-			return NULL;
-		if (buffer[ft_strlen(buffer) + 1] == '\n')
+		i = 0;
+		while (buffer[i] && buffer[i] != '\n')
+			i++;
+		if (buffer[i] == '\n')
 		{
 			bufflord(buffer);
-			break ;
+			return (line);
 		}
 		buffer[read(fd, buffer, BUFFER_SIZE)] = 0;
 	}
 	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char buffer[BUFFER_SIZE + 1] = {0};
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	size_t		i;
+	int			bytesread;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return NULL;
-	return (reader(fd, buffer));
+	if (fd < 0 || fd > FOPEN_MAX)
+	{
+		i = 0;
+		while (buffer[i])
+			buffer[i++] = 0;
+		return (NULL);
+	}
+	line = NULL;
+	if (!buffer[0])
+	{
+		bytesread = read(fd, buffer, BUFFER_SIZE);
+		if (bytesread < 0)
+			return (NULL);
+		buffer[bytesread] = 0;
+	}
+	line = reader(fd, buffer);
+	return (line);
 }
--- Exams/exam_03/get_next_line/get_next_line.h
+++ Exams/exam_03/get_next_line/get_next_line.h
@@ -1,13 +1,19 @@
 #ifndef GET_NEXT_LINE_H
-# define  GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+void	bufflord(char *str);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*reader(int fd, char *buffer);
 
 #endif
